a3f0fe95af3c0e6961cc25bbdd613e852adbbaf3
DenisBaicurov <denisbaicurov@mail.ru>
2023-09-20 20:31:30 +0400

    Выполнили 5 задание

---
+++ data.js
@@ -0,0 +1,22 @@
+export const datainfo=
+`[{"id" : "1",
+"name" : "Denis",
+"url" : "https://masterpiecer-images.s3.yandex.net/9009c34a570311eebeca02c370ea8a2f:upscaled"
+},
+
+{"id" : "2",
+"name" : "Dima",
+"url" : "https://i.postimg.cc/fRCTqLL8/nastol-com-ua-196225.jpg"
+},
+
+{"id" : "3",
+"name" : "Sasha",
+"url" : "https://i.postimg.cc/bvXr2MZh/nastol-com-ua-196231.jpg"
+},
+
+{"id" : "4",
+"name" : "Sergei",
+"url" : "https://i.postimg.cc/hjdg9857/nastol-com-ua-204076.jpg"
+}
+
+]`;
